7b35aa68c55dc5423395837ce33f1b55e51c7461
fernandozambone <fernando.zambone@gmail.com>
2024-03-28 22:22:42 -0500

    Added category router in main

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from routers.product_router import router as product_router
+from routers.category_router import router as category_router
 
 app = FastAPI()
 
 app.include_router(product_router)
+app.include_router(category_router)
 
 @app.get("/health")
 def health_check():
